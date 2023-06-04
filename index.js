d7b40a0c872cbc7cdc74cd0dbb2fbae679e752ca
MarkThunderous <mkhoroshylov2@gmail.com>
2023-06-04 06:24:42 +0300

    Update1

--- index.js
+++ index.js
@@ -167,6 +167,6 @@ app.post('/api/validate-coupon', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
